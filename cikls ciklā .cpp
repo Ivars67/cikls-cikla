71cc2fcf1080f2967e975d52aea8a07acdda3c5f
Ivars67 <ivars.kristapsons2008@gmail.com>
2025-09-19 10:11:43 +0300

    cikls ciklā .cpp

--- cikls ciklā .cpp
+++ cikls ciklā .cpp
@@ -6,9 +6,31 @@ int main() {
     
     for(int i=1; i<=5;i++){
     cout<<endl;
-    for(int i=1;i<=5;i++)
+    for(int j=1;j<=5;j++)
     
     cout<<i<<" ";
+        
+    }
+
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int r;
+    int k;
+    
+    cout<<"Ievadi rindu skaitu: "<<endl;
+    cin>>r;
+    cout<<"Ievadi kolonu skaitu"<<endl;
+    cin>>k;
+    
+    for(int i=1; i<=r;i++){
+    cout<<endl;
+    for(int j=1;j<=k;j++)
+    
+    cout<<j<<" ";
         
     }
 }
